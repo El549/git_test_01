adeb4df9f31e9d29780187102d5eda2a9c13ff11
HaoWish <77219072+HaoWish@users.noreply.github.com>
2022-10-03 17:56:09 +0800

    v0.51

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int pbq = 4;
 
-        int ph = 9;
-
+        int ph = 99999;
+        int ph ='hahhahaha';
         int x = pbq + ph;
 
         System.out.println(x);
